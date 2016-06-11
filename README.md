5d6496413627fecc53edb9fa1fe178d57f00da16
Xiaoliang Wang <xwang90@binghamton.edu>
2016-06-10 22:40:31 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+#TwitterTag
+
+Local unit test: Test from the command line  ./gradlew test
+                 Test from Android Studio
---
+++ TwitterHashtag/app/src/test/java/xlw/android/twitterhashtag/models/TweetTest.java
@@ -0,0 +1,28 @@
+package xlw.android.twitterhashtag.models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TweetTest {
+
+    @Test
+    public void verifyDefaults(){
+        Tweet tweet = new Tweet();
+
+        assertNull(tweet.PostDate);
+        assertNull(tweet.Status);
+        assertNull(tweet.StatusPic);
+        assertNull(tweet.UserName);
+        assertNull(tweet.UserPic);
+    }
+
+    @Test
+    public void canSetupTweet(){
+        Tweet tweet = new Tweet();
+
+        tweet.Status = "Status";
+
+        assertEquals(tweet.Status, "Status");
+    }
+}
